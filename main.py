328674a8ffa7601d5cad7cdcac23be56702d2545
kuramoriz <light-sveta-@mail.ru>
2020-03-19 17:39:30 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Привет от приложения Flask"
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+# внимание: заходить надо все равно по http://127.0.0.1:5000/
+
